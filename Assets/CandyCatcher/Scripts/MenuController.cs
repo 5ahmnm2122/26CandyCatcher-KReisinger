b7ca0ed071f807dce5814c210531fb51b2a57f1d
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2022-01-11 21:59:16 +0100

    dropdown

--- Assets/CandyCatcher/Scripts/MenuController.cs
+++ Assets/CandyCatcher/Scripts/MenuController.cs
@@ -9,7 +9,25 @@ public class MenuController : MonoBehaviour
     public UserData transferData;
     
     public Text playerNameInput;
+    public Dropdown modeSelect;
 
+    void Start() {
+        transferData.timeMode = true;
+        modeSelect.options.Clear();
+        modeSelect.captionText.text = "Modus wählen";
+        modeSelect.options.Add(new Dropdown.OptionData() {text = "Zeit Modus"});
+        modeSelect.options.Add(new Dropdown.OptionData() {text = "Endlos Modus"});
+        modeSelect.onValueChanged.AddListener(delegate{DropdownModeSelected(modeSelect);});
+    }
+
+    private void DropdownModeSelected(Dropdown dropdown) {
+        int index = dropdown.value;
+        if(dropdown.options[index].text == "Zeit Modus") {
+            transferData.timeMode = true;
+        } else {
+            transferData.timeMode = false;
+        }
+    }
     public void PlayGame() {
         transferData.playerName = playerNameInput.text;
         SceneManager.LoadScene("MainScene");
